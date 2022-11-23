9c66b2d2a27567be7676ae75cd9d3ec564d5c037
ZI JUNJIE Z00405ZE <junjie.zi@siemens.com>
2022-11-23 13:45:29 +0800

    init

---
+++ src/main/java/com/siemens/template/App.java
@@ -0,0 +1,15 @@
+package com.siemens.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Zi JunJie(junjie.zi@siemens.com)
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
